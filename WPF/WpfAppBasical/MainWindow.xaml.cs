4c13315f52793b2cc529f3504ac2cdf57f777ac2
TAR <764521788@qq.com>
2022-08-19 16:03:44 +0800

    WPFBasics

--- WPF/WpfAppBasical/MainWindow.xaml.cs
+++ WPF/WpfAppBasical/MainWindow.xaml.cs
@@ -23,6 +23,45 @@ namespace WpfAppBasical
         public MainWindow()
         {
             InitializeComponent();
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { UserName = "小王", ClassName = "高二三班", Address = "广州市" });
+            students.Add(new Student() { UserName = "小李", ClassName = "高三六班", Address = "清远市" });
+            students.Add(new Student() { UserName = "小张", ClassName = "高一一班", Address = "深圳市" });
+            students.Add(new Student() { UserName = "小黑", ClassName = "高一三班", Address = "赣州市" });
+            gd.ItemsSource = students;
+
+            List<Color> ColorList = new List<Color>();
+            ColorList.Add(new Color() { Code = "#FF8C00" });
+            ColorList.Add(new Color() { Code = "#FF7F50" });
+            ColorList.Add(new Color() { Code = "#FF6EB4" });
+            ColorList.Add(new Color() { Code = "#FF4500" });
+            ColorList.Add(new Color() { Code = "#FF3030" });
+            ColorList.Add(new Color() { Code = "#CD5B45" });
+
+            cob.ItemsSource = ColorList;
+            lib.ItemsSource = ColorList;
+
+            List<Test> tests = new List<Test>();
+            tests.Add(new Test() { Code = "1" });
+            tests.Add(new Test() { Code = "2" });
+            tests.Add(new Test() { Code = "3" });
+            tests.Add(new Test() { Code = "4" });
+            tests.Add(new Test() { Code = "6" });
+            ic.ItemsSource = tests;
         }
+    }
+    public class Student
+    {
+        public string UserName { get; set; }
+        public string ClassName { get; set; }
+        public string Address { get; set; }
+    }
+    public class Color
+    {
+        public string Code { get; set; }
+    }    
+    public class Test
+    {
+        public string Code { get; set; }
     }
 }
--- WPF/WpfControlLibrary/Calculator.xaml.cs
+++ WPF/WpfControlLibrary/Calculator.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,18 +11,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ControlLibrary
+namespace WpfControlLibrary
 {
     /// <summary>
-    /// SalaryCalculator.xaml 的交互逻辑
+    /// Calculator.xaml 的交互逻辑
     /// </summary>
-    public partial class SalaryCalculator : UserControl
+    public partial class Calculator : UserControl
     {
-        public SalaryCalculator()
+        public Calculator()
         {
             InitializeComponent();
         }
-
         private void Button_Click_Calcu(object sender, RoutedEventArgs e)
         {
             this.TextBox3.Text = this.TextBox2.Text + this.TextBox1.Text;
