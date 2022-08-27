574b12b1a1cd6d8d46dd43e5de81aadb742f9f92
TAR <764521788@qq.com>
2022-08-27 15:50:56 +0800

    WPF-Converter
    转换器：数据转换、拼接、传递等

---
+++ WPF/WpfAppExtend/Class/Converter/IDisplayConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfAppExtend.Class
+{/// <summary>
+/// 值转换器：将数值转换成对应类型
+/// </summary>
+    public class IDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value!=null)
+            {
+                string str= value.ToString();
+                if (str=="0")
+                {
+                    return "Yes";
+                }
+                else
+                {
+                    return "No";
+                }
+            }
+                return "Null";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPF/WpfAppExtend/Class/Converter/IMultiDisplayValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfAppExtend.Class.Converter
+{
+    public class IMultiDisplayValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length < 3)
+            {
+                return null;
+            }
+            byte r = System.Convert.ToByte(values[0]);
+            byte g = System.Convert.ToByte(values[1]);
+            byte b = System.Convert.ToByte(values[2]);
+            Color color = Color.FromRgb(r, g, b);
+            //SolidColorBrush solidColorBrush = new SolidColorBrush(color);
+            return new SolidColorBrush(color);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
