996191e078dffc40533e2ffda3739220425aeffa
TAR <764521788@qq.com>
2022-08-27 15:49:07 +0800

    WPF-Adorner
    装饰器：用于装饰控件外观，如头像角标等

---
+++ WPF/WpfAppExtend/Class/AdornerClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//必要引用
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WpfAppExtend
+{
+    //装饰器
+    public class AdornerClass : Adorner
+    {
+        public AdornerClass(UIElement adornedElement)
+    : base(adornedElement)
+        {
+
+        }
+        // A common way to implement an adorner's rendering behavior is to override the OnRender
+        // method, which is called by the layout system as part of a rendering pass.
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            Rect adornedElementRect = new Rect(this.AdornedElement.DesiredSize);
+
+            //// Some arbitrary drawing implements.
+            //SolidColorBrush renderBrush = new SolidColorBrush(Colors.Green);
+            //renderBrush.Opacity = 0.2;
+            //Pen renderPen = new Pen(new SolidColorBrush(Colors.Navy), 1.5);
+            //double renderRadius = 5.0;
+
+            //// Draw a circle at each corner.
+            //drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopLeft, renderRadius, renderRadius);
+            //drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopRight, renderRadius, renderRadius);
+            //drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomLeft, renderRadius, renderRadius);
+            //drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomRight, renderRadius, renderRadius);
+            Pen renderPen = new Pen(new SolidColorBrush(Colors.Red), 1.0);
+
+            // Draw a circle at each corner.
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.TopLeft.X - 3, adornedElementRect.TopLeft.Y - 3), new Point(adornedElementRect.TopLeft.X + 5, adornedElementRect.TopLeft.Y - 3));
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.TopLeft.X - 3, adornedElementRect.TopLeft.Y - 3), new Point(adornedElementRect.TopLeft.X - 3, adornedElementRect.TopLeft.Y + 5));
+
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.TopRight.X + 3, adornedElementRect.TopRight.Y - 3), new Point(adornedElementRect.TopRight.X - 5, adornedElementRect.TopRight.Y - 3));
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.TopRight.X + 3, adornedElementRect.TopRight.Y - 3), new Point(adornedElementRect.TopRight.X + 3, adornedElementRect.TopRight.Y + 5));
+
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.BottomLeft.X - 3, adornedElementRect.BottomLeft.Y + 3), new Point(adornedElementRect.BottomLeft.X + 5, adornedElementRect.BottomLeft.Y + 3));
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.BottomLeft.X - 3, adornedElementRect.BottomLeft.Y + 3), new Point(adornedElementRect.BottomLeft.X - 3, adornedElementRect.BottomLeft.Y - 5));
+
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.BottomRight.X + 3, adornedElementRect.BottomRight.Y + 3), new Point(adornedElementRect.BottomRight.X - 5, adornedElementRect.BottomRight.Y + 3));
+            drawingContext.DrawLine(renderPen, new Point(adornedElementRect.BottomRight.X + 3, adornedElementRect.BottomRight.Y + 3), new Point(adornedElementRect.BottomRight.X + 3, adornedElementRect.BottomRight.Y - 5));
+
+        }
+    }
+
+}
---
+++ WPF/WpfAppExtend/UserControl/Adornor.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppExtend.UserControl
+{
+    /// <summary>
+    /// Adornor.xaml 的交互逻辑
+    /// </summary>
+    public partial class Adornor /*: UserControl*/
+    {
+        public Adornor()
+        {
+            InitializeComponent();
+        }
+
+        private void AddAdorner(object sender, RoutedEventArgs e)
+        {
+            //layer.Add(new AdornerClass(TargetBtn));
+            //var layer = AdornerLayer.GetAdornerLayer(TargetBtn);
+            foreach (UIElement item in MyPanel.Children)
+            {
+                var layer = AdornerLayer.GetAdornerLayer(item);
+                layer.Add(new AdornerClass(item));
+            }
+        }
+
+        private void RemoveAdorner(object sender, RoutedEventArgs e)
+        {
+            foreach (UIElement item in MyPanel.Children)
+            {
+                var layer = AdornerLayer.GetAdornerLayer(item);
+                var arr = layer.GetAdorners(item);
+                if (arr != null)
+                {
+                    for (int i = arr.Length - 1; i >= 0; i--)
+                    {
+                        layer.Remove(arr[i]);
+                        //layer.Remove(new AdornerClass(arr[i]));
+                    }
+                }
+            }
+        }
+    }
+}
