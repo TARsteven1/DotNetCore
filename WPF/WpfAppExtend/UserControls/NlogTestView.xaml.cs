c55c6edc9ed30b1b9d7e896b9fa96bb423de58f1
TAR <764521788@qq.com>
2022-08-29 17:36:26 +0800

    WPF-Nlog
    异常输出功能，比我自己写的好用

---
+++ WPF/WpfAppExtend/UserControls/NlogTestView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppExtend.UserControls
+{
+    /// <summary>
+    /// NlogTestView.xaml 的交互逻辑
+    /// </summary>
+    public partial class NlogTestView : UserControl
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public NlogTestView()
+        {
+            InitializeComponent();
+
+            //自发生成异常
+            logger.Info("这是一个异常！");
+            logger.Warn("这是一个异常！");
+            logger.Debug("123");
+            logger.Debug("11111111111111111");
+            logger.Debug("2");
+            //Console.ReadKey();
+        }
+    }
+}
