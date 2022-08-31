3f30a37f0320911dd263a1110f7258e6f2093629
TAR <764521788@qq.com>
2022-08-31 20:48:06 +0800

    WPFPrism-CreatePrismProgram
    如何将普通WPF项目转换成Prism项目

---
+++ WPF/WpfPrism/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
+
+namespace WpfPrism
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            //使用官方提供的容器拿到主窗口并返回
+            return Container.Resolve<MainWindow>();
+        }
+        //使用容器的服饰来注册我们要使用的页面，依赖或者服务
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
