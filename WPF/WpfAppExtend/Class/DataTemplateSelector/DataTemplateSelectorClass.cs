cdab8c51a69737db3171458f98129148183e015d
TAR <764521788@qq.com>
2022-08-31 10:17:39 +0800

    WPF-DataTemplateSelector
    数据模板选择器，根据不同的数据展现不同的模板

---
+++ WPF/WpfAppExtend/Class/DataTemplateSelector/DataTemplateSelectorClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfAppExtend.UserControls;
+
+namespace WpfAppExtend.Class.DataTemplateSelector
+{
+    class DataTemplateSelectorClass : System.Windows.Controls.DataTemplateSelector
+    {
+        public DataTemplate HightTemplate { get; set; }
+        public DataTemplate LowTemplate { get; set; }
+
+        //根据不同的数据展现不同的模板
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var stu = (Student)item;
+            if (stu.Score>60)
+            {
+                return HightTemplate;
+
+            }
+            else
+            {
+                return LowTemplate;
+            }
+            //return base.SelectTemplate(item, container);
+        }
+    }
+}
---
+++ WPF/WpfAppExtend/UserControls/DataTemplateSelectorControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppExtend.UserControls
+{
+    /// <summary>
+    /// DataTemplateSelectorControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataTemplateSelectorControl : UserControl
+    {
+        public DataTemplateSelectorControl()
+        {
+            InitializeComponent();
+            List<Student> stuList = new List<Student>();
+            stuList.Add( new Student() { Name = "Tar", Score = 40 });
+            stuList.Add( new Student() { Name = "Tar1", Score = 50 });
+            stuList.Add( new Student() { Name = "Tar2", Score = 60 });
+            stuList.Add( new Student() { Name = "Tar3", Score = 70 });
+
+            list.DataContext = new
+            {
+                Model = stuList
+            };
+        }
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
