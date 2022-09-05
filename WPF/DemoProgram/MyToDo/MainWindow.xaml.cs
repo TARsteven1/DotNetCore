142ea86806b7427cf41cf61cdb69438484689e69
TAR <764521788@qq.com>
2022-09-05 14:52:43 +0800

    WPFDemo-设计顶部导航条

--- WPF/DemoProgram/MyToDo/MainWindow.xaml.cs
+++ WPF/DemoProgram/MyToDo/MainWindow.xaml.cs
@@ -23,6 +23,24 @@ namespace MyToDo
         public MainWindow()
         {
             InitializeComponent();
-        }
+            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+            btnMax.Click += (s, e) => { if (this.WindowState == WindowState.Maximized) this.WindowState = WindowState.Normal;
+                else this.WindowState = WindowState.Maximized;
+            };
+            btnClose.Click += (s, e) => { this.Close(); };
+
+            TopColorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+            TopColorZone.MouseDoubleClick += (s, e) =>
+             {
+                 if (this.WindowState == WindowState.Normal) this.WindowState = WindowState.Maximized;
+                 else this.WindowState = WindowState.Normal;
+             };
+            }
     }
 }
