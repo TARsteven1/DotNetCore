079ab38f2cdb485cc52fe7777eb77c7ebbab9117
TAR <764521788@qq.com>
2022-09-08 20:01:29 +0800

    WPFDemo-设计备忘录界面
    备忘录跟待办界面差不多,添加测试数据,实现了打开页面内部数据项渐入的动画效果（更多效果请参考materialDesign中的Transition）

--- WPF/DemoProgram/MyToDo/ViewModels/MemoViewModel.cs
+++ WPF/DemoProgram/MyToDo/ViewModels/MemoViewModel.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using Prism.Commands;
+using MyToDo.Common.Models;
 
 namespace MyToDo.ViewModels
 {
-    public class MemoViewModel
+    public class MemoViewModel : BindableBase
     {
+        public MemoViewModel()
+        {
+            MemoDtos = new ObservableCollection<MemoDTO>();
+            CreateMemoList();
+            OpenRightDrawerCommand = new DelegateCommand(() => { IsRightDrawerOpen = !IsRightDrawerOpen; });
+        }
+        private ObservableCollection<MemoDTO> memoDtos;
+
+        public ObservableCollection<MemoDTO> MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; RaisePropertyChanged(); }
+        }
+        void CreateMemoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                MemoDtos.Add(new MemoDTO() { Title = "测试数据" + i, Content = "嘻嘻嘻嘻嘻嘻寻寻"/*, Color = "#FF0CA0FF" */});
+            }
+
+        }
+        public DelegateCommand OpenRightDrawerCommand { private set; get; }
+        private bool isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
     }
 }
