2434d7a3856484b400c29cd37710df243b6d58c1
TAR <764521788@qq.com>
2022-09-08 19:01:48 +0800

    WPFDemo-设计待办界面
    实现待办界面,添加测试数据,实现点击按钮弹出侧边栏

--- WPF/DemoProgram/MyToDo/ViewModels/ToDoViewModel.cs
+++ WPF/DemoProgram/MyToDo/ViewModels/ToDoViewModel.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using Prism.Commands;
+using MyToDo.Common.Models;
 
 namespace MyToDo.ViewModels
 {
-   public class ToDoViewModel
+   public class ToDoViewModel:BindableBase
     {
+        public ToDoViewModel()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDTO>();
+            CreateToDoList();
+            OpenRightDrawerCommand = new DelegateCommand(()=> { IsRightDrawerOpen = !IsRightDrawerOpen; });
+        }
+        private ObservableCollection<ToDoDTO> toDoDtos;
+
+        public ObservableCollection<ToDoDTO> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged(); }
+        }
+        void CreateToDoList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+
+            ToDoDtos.Add(new ToDoDTO() {  Title = "测试数据"+i, Content = "嘻嘻嘻嘻嘻嘻寻寻"/*, Color = "#FF0CA0FF" */});
+            }
+
+        }
+        public DelegateCommand OpenRightDrawerCommand { private set; get; }
+        private bool isRightDrawerOpen;
+
+        public bool IsRightDrawerOpen
+        {
+            get { return isRightDrawerOpen; }
+            set { isRightDrawerOpen = value;  RaisePropertyChanged(); }
+        }
+
+
     }
 }
