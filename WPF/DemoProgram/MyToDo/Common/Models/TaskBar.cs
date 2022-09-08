f5b4d08b2db303b33046654c0e6cac44f26fc162
TAR <764521788@qq.com>
2022-09-08 09:40:11 +0800

    WPFDemo-首页基础设计
    实现首页基础的静态UI,涉及数据模版,Canvas内图形裁剪效果,界面布局等知识点

---
+++ WPF/DemoProgram/MyToDo/Common/Models/TaskBar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.Common.Models
+{/// <summary>
+/// 首页中部按钮菜单实体类
+/// </summary>
+   public class TaskBar
+    {
+        private string icon;
+
+        public string Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        private string count;
+
+        public string Count
+        {
+            get { return count; }
+            set { count = value; }
+        }        
+        private string color;
+
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        private string target;
+
+        public string Target
+        {
+            get { return target; }
+            set { target = value; }
+        }
+    }
+}
--- WPF/DemoProgram/MyToDo/ViewModels/IndexViewModel.cs
+++ WPF/DemoProgram/MyToDo/ViewModels/IndexViewModel.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using MyToDo.Common.Models;
 
 namespace MyToDo.ViewModels
 {
-    public class IndexViewModel
+    public class IndexViewModel:BindableBase
     {
+        public IndexViewModel()
+        {
+            TaskBars = new ObservableCollection<TaskBar>();
+            CreateTaskBar();
+        }
+        private ObservableCollection<TaskBar> taskBars;
+
+        public ObservableCollection<TaskBar> TaskBars
+        {
+            get { return taskBars; }
+            set { taskBars = value; RaisePropertyChanged(); }
+        }
+        void CreateTaskBar()
+        {
+            TaskBars.Add(new TaskBar() { Icon = "ClockFast", Title = "汇总", Target = "IndexView" ,Color="#FF0CA0FF",Count="9"});
+            TaskBars.Add(new TaskBar() { Icon = "ClockCheckOutline", Title = "已完成", Target = "ToDoView", Color = "#FF1ECA3A", Count = "9" });
+            TaskBars.Add(new TaskBar() { Icon = "ChartLineVariant", Title = "完成比例", Target = "MemoView", Color = "#FF02C6DC", Count = "100%" });
+            TaskBars.Add(new TaskBar() { Icon = "PlaylistStar", Title = "备忘录", Target = "SettingsView", Color = "#FFFFA000", Count = "19" });
+        }
     }
 }
