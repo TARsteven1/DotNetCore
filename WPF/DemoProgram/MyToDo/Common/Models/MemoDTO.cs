7c20a9ca5c656a6efcaa903023590d951fff3789
TAR <764521788@qq.com>
2022-09-08 13:30:06 +0800

    WPFDemo-首页填充测试数据
    创建TODO数据模型,增加边框高亮特效

---
+++ WPF/DemoProgram/MyToDo/Common/Models/MemoDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.Common.Models
+{
+    /// <summary>
+    /// 备忘录实体类
+    /// </summary>
+   public  class MemoDTO :BaseDTO
+    {
+        private string content;
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        private string status;
+
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+    }
+}
--- WPF/DemoProgram/MyToDo/ViewModels/IndexViewModel.cs
+++ WPF/DemoProgram/MyToDo/ViewModels/IndexViewModel.cs
@@ -9,12 +9,22 @@ using MyToDo.Common.Models;
 
 namespace MyToDo.ViewModels
 {
-    public class IndexViewModel:BindableBase
+    public class IndexViewModel : BindableBase
     {
+        private string info;
+
+        public string Info
+        {
+            get { return "今天是"+System.DateTime.Now.ToString("yyyy-MM-dd")+" "+ System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek); }
+            set { info = value; RaisePropertyChanged(); }
+        }
         public IndexViewModel()
         {
             TaskBars = new ObservableCollection<TaskBar>();
             CreateTaskBar();
+
+            ToDoDtos = new ObservableCollection<ToDoDTO>();
+            MemoDtos = new ObservableCollection<MemoDTO>();
         }
         private ObservableCollection<TaskBar> taskBars;
 
@@ -25,10 +35,25 @@ namespace MyToDo.ViewModels
         }
         void CreateTaskBar()
         {
-            TaskBars.Add(new TaskBar() { Icon = "ClockFast", Title = "汇总", Target = "IndexView" ,Color="#FF0CA0FF",Count="9"});
+            TaskBars.Add(new TaskBar() { Icon = "ClockFast", Title = "汇总", Target = "IndexView", Color = "#FF0CA0FF", Count = "9" });
             TaskBars.Add(new TaskBar() { Icon = "ClockCheckOutline", Title = "已完成", Target = "ToDoView", Color = "#FF1ECA3A", Count = "9" });
             TaskBars.Add(new TaskBar() { Icon = "ChartLineVariant", Title = "完成比例", Target = "MemoView", Color = "#FF02C6DC", Count = "100%" });
             TaskBars.Add(new TaskBar() { Icon = "PlaylistStar", Title = "备忘录", Target = "SettingsView", Color = "#FFFFA000", Count = "19" });
+        }
+
+        private ObservableCollection<ToDoDTO> toDoDtos;
+
+        public ObservableCollection<ToDoDTO> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged(); }
+        }        
+        private ObservableCollection<MemoDTO> memoDtos;
+
+        public ObservableCollection<MemoDTO> MemoDtos
+        {
+            get { return memoDtos; }
+            set { memoDtos = value; RaisePropertyChanged(); }
         }
     }
 }
