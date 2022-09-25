e33fb2f6e927a286f6ee6ddcfd6b257db720a7de
XIUXIU <2270677197@qq.com>
2022-09-25 19:49:40 +0800

    C#-Tools-BenchmarkDotNet性能测试
    使用BenchmarkDotNet测试一个BMT类型的方法性能

---
+++ C#/Other/BenchmarkDotNet 测试程序性能/TryBenchmarkDotNet/Program.cs
@@ -0,0 +1,52 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Text.RegularExpressions;
+
+namespace TryBenchmarkDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("使用BenchmarkDotNet进行性能测试!");
+            //运行后会让系统运行至最佳状态再去进行性能测试
+            BenchmarkRunner.Run<BMT>();
+            Console.Write("按任意键退出...");
+            Console.ReadKey(true);
+
+        }
+    }
+   /// <summary>
+   /// 用三个函数进行性能比较
+   /// </summary>
+   /// 内存监听
+    [MemoryDiagnoser]
+    public class BMT
+    {
+        private string _str = "林柳 蓝鲸 一双混";
+        [Benchmark]
+        public int Find1() {
+            //正则表达式
+            Regex regex = new(@"\b\w+\b");
+            return regex.Matches(_str).Count;
+        }        
+            Regex regex1 = new(@"\b\w+\b");
+        [Benchmark]
+        public int Find2() {
+            return regex1.Matches(_str).Count;
+        } 
+        [Benchmark]
+        public int Find3() {
+            //使用静态搜索
+            return Regex.Matches(_str, @"\b\w+\b").Count;
+        }       
+        [Benchmark]
+        public int Find4() {
+            //主动申请一些内存
+            var test = new int[10];
+            //使用静态搜索
+            return Regex.Matches(_str, @"\b\w+\b").Count;
+        }
+    }
+}
