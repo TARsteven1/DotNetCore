fed53883e27ef44901a53ff91f8198e0cd073db5
XIUXIU <2270677197@qq.com>
2022-09-28 15:19:58 +0800

    C#-异步多线程-线程基础
    创建线程，线程基础属性等

---
+++ C#/异步多线程/ThreadTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteY);//开辟新线程,将委托传入线程中
+            thread.Name = "Y Thread...";
+            thread.Start();//开始运行线程
+
+            thread.Join();//等待thread线程结束后再继续后续代码，有2个重载，可用于判断线程超时
+            Thread.Sleep(4000);//暂停当前线程，等待4秒
+            Thread.Yield();//等于 Thread.Sleep(0);用于诊断
+
+
+            //同时主线程也再工作
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("X");
+            }
+        }
+        static void WriteY()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Y");
+            }
+        }
+        static void Doc()
+        {
+            //线程的一些属性说明
+            Thread thread = new Thread(WriteY);
+
+            //一旦开始就是true，线程结束就是fasle（传入线程的委托结束后），线程结束后无法重启
+            Console.WriteLine(thread.IsAlive);
+            //线程名，用于调试，只能设置一次，再改回抛出异常
+            thread.Name = "Y Thread...";
+            //静态属性，返回当前执行的线程
+            Console.WriteLine(Thread.CurrentThread);
+            //线程的状态
+            Console.WriteLine(thread.ThreadState);
+
+
+
+        }
+    }
+}
