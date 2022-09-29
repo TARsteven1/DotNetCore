441b5865dee71c0b0766fc240b39179bbb01f838
XIUXIU <2270677197@qq.com>
2022-09-29 14:21:27 +0800

    C#-异步多线程-线程传参&异常处理

---
+++ C#/异步多线程/ThreadTest/2ThreadParameter/How/ParameterPassing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadTest._2ThreadParameter.How
+{
+    class ParameterPassing
+    {
+        #region 最简单的传参方法（使用Lambda表达式）
+        static void TheMostEasyWay2PassPara2Thread()
+        {
+            Thread t = new Thread(() => Print("Pass String"));
+            t.Start();
+            //也可以整个都写在Lambda里
+            new Thread(() => { Console.WriteLine("Pass String Aging"); }).Start();
+        }
+        static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+        #endregion
+
+        #region C#3.0前
+        static void LastCSharp3Dot0()
+        {
+            //3.0以前没有Lambda
+            Thread t = new Thread(Print1);
+            t.Start("Pass String");//在此传参
+        }
+        static void Print1(object str)
+        {
+            string temp = (String)str;
+            Console.WriteLine(temp);
+        }
+        //Thread的重载构造函数可接受下列两个委托之一作为参数：
+        //public delegate void ThreadStart();
+        //public delegate void ParameterizedThreadStart(object obj);
+        #endregion
+
+        #region 如何保证Lambda不修改被捕获的变量？（造成的原因是见：1ThreadSafety-Why-52行）
+        static void KeepValueInLambda()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int temp = i;//Key Point
+                new Thread(() => { Console.Write(temp); }).Start();
+            }
+            //这样输出的结果就不会被修改，但顺序是乱的（输出1-10）
+        }
+        #endregion
+    }
+}
---
+++ C#/异步多线程/ThreadTest/3ThreadException/ThreadException.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadTest._3ThreadException
+{
+    class ThreadException
+    {
+        #region 正常情况下无法捕获到线程异常
+        static void Main()
+        {
+            try
+            {
+                new Thread(Go).Start();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("正常情况下无法捕获到线程异常");
+            }
+        }
+        static void Go()
+        {
+            throw null;//主动抛出一个空异常
+        }
+        #endregion
+
+        #region 如何捕获线程异常
+        static void HowToCatchThreadException()
+        {
+            new Thread(Go1).Start();
+
+        }
+        static void Go1()
+        {
+            try
+            {
+                throw null;//主动抛出一个空异常
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("捕获异常"+ e.Message);
+            }
+        }
+        #endregion
+
+        #region 其他
+        //在 WPF、WinForm中可通过订阅全局异常处理事件：(但只能处理UI线程的异常，否则不会触发这些异常)
+            //Application.DispatcherUnhandledException
+            //Application.ThreadException
+        //而任何线程由未处理的异常都会触发：
+           // AppDomain.CurrentDomain.UnhandledException
+        #endregion
+    }
+}
