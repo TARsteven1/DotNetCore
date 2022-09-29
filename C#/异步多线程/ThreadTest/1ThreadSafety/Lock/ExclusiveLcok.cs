4781dccaf5870726c8c89aa73db5b193e8742a54
XIUXIU <2270677197@qq.com>
2022-09-29 12:29:21 +0800

    C#-异步多线程-线程安全
    线程安全的由来，使用排它锁维护线程安全

---
+++ C#/异步多线程/ThreadTest/1ThreadSafety/Lock/ExclusiveLcok.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadTest._1ThreadSafety.Lock
+{
+    class ExclusiveLcok
+    {
+        static bool _done;
+        static readonly object _locker = new object();
+
+        static void Main()
+        {
+            new Thread(Go).Start();
+            Go();
+        }
+        static void Go()
+        {//使用lock语句加锁
+            lock (_locker)//锁可以给予任何引用类型对象
+            {
+                if (!_done)
+                {
+                    Console.WriteLine("Done");
+                    _done = true;
+                }
+            }
+            //当人多个线程同时竞争一个锁的时候，一个线程回等待或阻塞，直至锁变成可用状态
+            //在多线程上下文中，以此种方式避免避免不确定性的代码就叫 线程安全
+            //lock不是线程安全的银弹，容易忘记对字段加锁，lock也会引起死锁问题
+        }
+    }
+}
---
+++ C#/异步多线程/ThreadTest/1ThreadSafety/Why/LocalvsSharedState.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+
+namespace ThreadTest._1ThreadSafety.Why
+{
+    class LocalvsSharedState
+    {
+        static void Main()
+        {
+
+        }
+        #region Local本地独立：CLR为每个线程分配自己的内存栈（Stack），以便本地变量保持独立
+        static void Local()
+        {
+            new Thread(Go).Start();//新线程调用Go
+            Go();
+        }
+        static void Go()
+        {
+            //cycles是本地变量
+            //再每个线程的内存栈上都会创建cycles的独立副本
+            for (int cycles = 0; cycles < 5; cycles++)
+            {
+                Console.WriteLine("？");
+            }
+        }
+        //结果输出10个？
+        #endregion
+
+        #region Shared共享1：多个线程共享同一个实例
+        bool _done;
+        static void SharedInstance()
+        {
+            LocalvsSharedState lvs=  new LocalvsSharedState();//创建共有的实例
+            new Thread(lvs.Run).Start();//新线程调用Run
+
+            lvs.Run();
+        }
+         void Run()//实例方法
+        {
+            if (!_done)
+            {
+                _done = true;
+                Console.WriteLine("Done");
+            }
+        }
+        //结果只显示一个Done，两个线程使用共用的实例调用的方法，所以使用的是同一个_done
+        #endregion
+        
+        #region Shared共享2：被Lambda表达式或匿名委托所捕获的本地变量，会被编译器转化为字段(field),所以也会被共享
+        static void SharedDelegate()
+        {
+            bool done=false;
+            ThreadStart action = () =>
+            {
+                if (!done)
+                {
+                    done = true;
+                    Console.WriteLine("Done");
+                }
+            };
+            new Thread(action).Start();//新线程调用Run
+
+            action();
+        }
+        //结果只显示一个Done
+        #endregion
+
+        #region Shared共享3：静态字段(field)也会在线程间共享数据
+           static bool done;//静态字段在同一个作用域下被所有线程共享
+        static void SharedStaticField()
+        {
+
+            new Thread(Run1).Start();//新线程调用Run
+
+            Run1();
+        }
+        static void Run1()//实例方法
+        {
+            if (!done)
+            {
+                done = true;
+                Console.WriteLine("Done");
+            }
+        }
+        //结果只显示一个Done
+        #endregion
+    }
+
+    //结论：Shared共享状态缺乏线程安全，由于输出无法确定（有可能输出两次Done），尽量避免使用共享状态
+}
